c6e95df2be93c4ae125582eabdaa6dd8a2a5b90a
Vagner Zampieri <vgzampieri@gmail.com>
2012-06-16 15:42:19 -0300

    start project

---
+++ spec/models/app_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../app.rb'
+require 'rack/test'
+
+set :environment, :test
+
+describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/'
+    last_response.should be_ok
+    last_response.body.strip.should == 'TESTE'
+  end
+end
